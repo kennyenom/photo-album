4b1eb3c21fe6cec6150450391d24bef0a7d97910
Your Name <you@example.com>
2022-02-18 03:12:56 +0300

    updated commit

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -120,9 +120,17 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+MEDIA_ROOT = BASE_DIR / 'static/images'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_URL = '/images/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
