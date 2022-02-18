897dc30ae5799a722cdb2a175c1eeee2d55d7b4f
Your Name <you@example.com>
2022-02-18 03:21:06 +0300

    updated commit

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w@pkra77-zh0s-l0=%!%p*0l97$^&_lnbjyzizwvm%9y+3@p&a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['enomjey.herokuapp.com','127.0.0.1']
 
 
